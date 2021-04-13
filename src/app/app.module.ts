cd6a3edfe7a6927d56d325054b17450cd27383af
yuichi <20w2100d@shinshu-u.ac.jp>
2021-04-14 02:01:52 +0900

    HttpModule の import 宣言 HttpModule を imports プロパティの要素に登録して利用を宣言

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,26 @@ import { BooksComponent } from './books/books.component';
 import { FormsModule} from '@angular/forms';
 import { BookDetailComponent } from './book-detail/book-detail.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientComponent } from './http-client/http-client.component';
+import { HttpClientService } from './service/http-client.service';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     BooksComponent,
     BookDetailComponent,
+    HttpClientComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    HttpClientService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
